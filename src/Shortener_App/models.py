07ad399e7b01f98687a23b689f7dd2df5633d711
Bartłomiej Borecki <borecki.bartek@gmail.com>
2019-07-03 22:09:24 +0200

    extended models

--- src/Shortener_App/models.py
+++ src/Shortener_App/models.py
@@ -2,5 +2,14 @@ from django.db import models
 
 # Create your models here.
 class JustURL(models.Model):
-    input_url = models.CharField(max_length=200)
+    input_url = models.CharField(max_length=256)
+    short_url = models.CharField(max_length=24, unique=True, blank=True)
+    active = models.BooleanField(default=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, blank=True, null=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=512)
+
 
