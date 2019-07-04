00feab0d6458f5eca57a595fc5ea7cdf369a6fdf
Bartłomiej Borecki <borecki.bartek@gmail.com>
2019-07-04 11:10:59 +0200

    added input url validator

--- src/Shortener_App/forms.py
+++ src/Shortener_App/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import JustURL, Category
-
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+import re
 
 class ShortUrlForm(forms.ModelForm):
     category = forms.ModelChoiceField(queryset=Category.objects.all(), required=False)
@@ -12,9 +14,23 @@ class ShortUrlForm(forms.ModelForm):
             'category'
         ]
 
+    def clean_input_url(self):
+        url = self.cleaned_data['input_url']
+        if url.startswith('http://') or url.startswith('www.'):
+            result = url
+        else:
+            result = 'http://' + url
+
+        if not url.endswith(('.com', '.pl', '.de', '.uk')):
+            result += '.com'
+        try:
+            URLValidator(result)
+        except:
+            raise ValidationError('Invalid URL!')
+        return result
+
 
 class CategoryModelForm(forms.ModelForm):
-
     class Meta:
         model = Category
         fields = [
@@ -26,6 +42,5 @@ class CategoryModelForm(forms.ModelForm):
         )
         widgets = {
             'description': forms.Textarea(attrs={'cols': 40, 'rows': 15},
-                                            ),
+                                          ),
         }
-
